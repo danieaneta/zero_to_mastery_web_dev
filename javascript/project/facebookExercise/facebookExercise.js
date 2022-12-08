8d0f56cf7faf955a3574e4dd7cf30b789577e918
danieaneta <83565907+danieaneta@users.noreply.github.com>
2022-12-07 22:01:38 -0600

    Create facebookExercise.js

---
+++ javascript/project/facebookExercise/facebookExercise.js
@@ -0,0 +1,39 @@
+// Create an object and an array which we will use in our facebook exercise. 
+
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+
+
+// 2. Create an array which contains the object you have made above and name the array "database".
+
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+
+//==========================================================================================================
+
+var user = {
+  username: "Danie",
+  password: "12345",
+} 
+
+database = [
+  {
+    user,
+  }
+]
+
+var 
+
+newsfeed = [
+  {
+    username: "danie",
+    timeline: "timeline01",
+  },
+  {
+    username: "joy",
+    timeline: "timeline02",
+  },
+  {
+    username: "al",
+    timeline: "timeline03",
+  }
+]
+
