5ca9fb188238cb3bad929181834e34a98d03e7ab
danieaneta <83565907+danieaneta@users.noreply.github.com>
2022-12-07 22:14:46 -0600

    Update facebookExercise.js

--- javascript/project/facebookExercise/facebookExercise.js
+++ javascript/project/facebookExercise/facebookExercise.js
@@ -44,3 +44,16 @@ newsfeed = [
   }
 ]
 
+var userNamePrompt = prompt("What's your username?");
+var passwordPrompt = prompt("What's your password?");
+
+function signIn(user, pass) {
+  if (user === database[0].username && 
+      pass === database[0].password) {
+      console.log(newsFeed);
+  } else {
+      alert("Sorry, wrong username or password.")
+  }    
+}
+
+signIn(userNamePrompt, passwordPrompt);
